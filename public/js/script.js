3962fbe142ccf74a0083af61b939fdd7c3ccaf85
Le5ya <liliatolstyh@yandex.ru>
2024-08-24 17:40:23 +0300

    remaining seconds

--- public/js/script.js
+++ public/js/script.js
@@ -13,6 +13,12 @@ const fetchBusData = async () => {
 const formatDate = (date) => date.toISOString().split("T")[0];
 const formatTime = (date) => date.toTimeString().split(" ")[0].slice(0, 5);
 
+const getTimeRemainingSeconds = (departureTime) => {
+  const now = new Date();
+  const timeDeference = departureTime - now;
+  return Math.floor(timeDeference / 1000);
+};
+
 const renderBusData = (buses) => {
   const tableBody = document.querySelector("#bus tbody");
   tableBody.textContent = "";
@@ -23,12 +29,18 @@ const renderBusData = (buses) => {
     const nextDepartureDateTimeUTC = new Date(
       `${bus.nextDeparture.date}T${bus.nextDeparture.time}Z`
     );
+
+    const remainingSeconds = getTimeRemainingSeconds(nextDepartureDateTimeUTC);
+
+    const remainingTimeText =
+      remainingSeconds < 60 ? "Отправляется" : bus.nextDeparture.remaining;
+
     row.innerHTML = `
 		<td>${bus.busNumber}</td>
 		<td>${bus.startPoint} - ${bus.endPoint}</td>
 		<td>${formatDate(nextDepartureDateTimeUTC)}</td>
 		<td>${formatTime(nextDepartureDateTimeUTC)}</td>
-		<td>${bus.nextDeparture.remaining}</td>
+		<td>${remainingTimeText}</td>
 		`;
     tableBody.append(row);
   });
@@ -51,12 +63,21 @@ const initWebSocket = () => {
     console.log(`Websocket connection close`);
   });
 };
+const updateTime = () => {
+  const currentTimeElement = document.querySelector("#current-time");
+  const now = new Date();
+  currentTimeElement.textContent = date.toTimeString().split(" ")[0];
+
+  setTimeout(updateTime, 1000);
+};
 
 const init = async () => {
   const buses = await fetchBusData();
   renderBusData(buses);
 
   initWebSocket();
+
+  updateTime();
 };
 
 init();
